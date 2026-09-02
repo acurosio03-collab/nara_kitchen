78978dae995efc92abe949a90cf9e03fb75e1dee
acurosio03-collab <acurosio03@gmail.com>
2026-09-03 00:33:21 +0700

    Delete js/supabase-config.js

--- js/supabase-config.js
+++
@@ -1,3 +0,0 @@
-// Supabase configuration for NARA KITCHEN
-window.SUPABASE_URL = "https://wwnrcdotwjxgterrhprm.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_vWecM2H8Gvurp3v4EchPRg_WCexKyqk";
